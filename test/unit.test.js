69fb6588dd8d445f74d9bfa24f998ad8a1b1cb47
alrasheedftah <alrasheedrasta@gmail.com>
2022-09-17 02:57:36 +0400

    add Unit Test For Method

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,9 +1,13 @@
-const { isCsvFile, fileExists } = require('../functions');
+const { isCsvFile, fileExists, validArgsCount, validIndexColumn } = require('../functions');
 
 // before(async() => {
-    process.argv.push(['ana.csv',0,2])
+    process.argv.push('ana.csv')
+    process.argv.push('0')
+    process.argv.push('1')
     let filePath = "ana.csv"
 // })
+
+
 
 describe(" Test For Functionalit Of  File  ",()=>{
 
@@ -19,6 +23,28 @@ describe(" Test For Functionalit Of  File  ",()=>{
 
     it("Check Method FileExsits  is Founds  ",async()=>{
         expect(fileExists(filePath)).toBeDefined();
+    })
+
+    it("Check File Exsit Or Not ",async()=>{
+        let res = await fileExists(filePath);
+        expect(res).toBe(true);
+    })
+
+})
+
+
+
+describe(" Test For Arguments  ",()=>{
+
+    it("check the Args Counts Is Valid   ",async()=>{
+        let res = await validArgsCount(process.argv.length);
+        expect(res).toBe(true);
+        // expect(Object.is(isCsvFile(filePath), true)).toBe(false)
+    })
+
+    it("Check The 2sc argument is Been Index  ",async()=>{
+        let res = await validIndexColumn(process.argv[3]);
+        expect(res).toBe(true);
     })
 
     it("Check File Exsit Or Not ",async()=>{
